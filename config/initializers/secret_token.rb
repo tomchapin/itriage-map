647df5be2620ce3e3c494bd041ca4b9782aee6a2
Tom Chapin <tchapin@gmail.com>
2013-07-18 12:03:16 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItriageMap::Application.config.secret_token = '9f969dc32c7dd055f1d22d3d8dac943987b8d0aca6801056cb2e78ff229b16cad20f85a4c24ffeaf972a5c4664fbff34f00c281c6f1ea0121da133e4397d70ff'
